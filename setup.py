4417cee441aaefbb94e4a39f2e06187a767a9cb7
joegruss <joe.gruss@lifecanvastech.com>
2022-06-24 12:42:55 -0400

    first commit

--- setup.py
+++ setup.py
@@ -1,40 +1,32 @@
 from setuptools import setup
 
-# reading long description from file
-with open('DESCRIPTION.txt') as file:
+VERSION = "1.0.0"
+
+with open('README.md') as file:
     long_description = file.read()
 
+REQUIREMENTS = ['pystripe', 'os', 'multiprocessing', 'configparser', 'pathlib', 'csv', 'tkinter', 'sys']
 
-# specify requirements of your package here
-REQUIREMENTS = ['requests']
 
 # some more details
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'Topic :: Internet',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    ]
+    'Programming Language :: Python :: 3.7',
+]
 
 # calling the setup function 
-setup(name='mygmap',
-      version='1.0.3',
-      description='A small wrapper around google maps api',
+setup(name='destripegui',
+      version=VERSION,
+      description='A GUI for automatic pystripe destriping',
       long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/nikhilkumarsingh/mygmap',
-      author='Nikhil Kumar Singh',
-      author_email='nikhilksingh97@gmail.com',
+      author='LifeCanvas Technologies',
       license='MIT',
-      packages=['geo'],
+      packages=['pystripe'],
       classifiers=CLASSIFIERS,
       install_requires=REQUIREMENTS,
-      keywords='maps location address'
-      )
+      entry_points={
+        'console_scripts' : ['destripegui=src.destripegui:main']
+      }
+)
