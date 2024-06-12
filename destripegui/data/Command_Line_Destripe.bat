3e1ec8f5e0ccaef6620605cd7f3e98b792e94a6f
joegruss <joe.gruss@lifecanvastech.com>
2024-06-12 12:25:05 -0400

    window resizing from batch file

--- destripegui/data/Command_Line_Destripe.bat
+++ destripegui/data/Command_Line_Destripe.bat
@@ -1,3 +1,10 @@
+@echo off
+for /f "tokens=4 delims=[] " %%a in ('ver') do @set version=%%a
+
+if %version:~5,2%==19 mode con: cols=120 lines=60
+@echo on
+@echo Loading Destriper...
+@echo off
+
 call activate command_line_destripe
 command_line_destripe
-pause
